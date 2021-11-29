bb479476dd4cec51cc857c3ecdf111d3166aa4f6
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2021-11-29 15:21:14 -0500

    Updated projects section

--- index.php
+++ index.php
@@ -38,24 +38,7 @@
         <main class="home_container">
             <?php require_once 'part/home/hero.php' ?>
 
-            <section class="home__section text-center">
-                <div class="container container--narrow">
-                    <h2 class="fs-4 mb-2 fw-500 sidelines">Projets personnels</h2>
-                    <div class="row mt-5">
-                        <div class="col-1-2 border-right">
-                            <h3 class="my-0 fw-500">Application mobile d'entrainement</h3>
-                            <div class="">App React, vue mobile seulement</div>
-                            <a class="btn-primary mt-3" target="_blank" href="http://trainingapp.williamlafortunecaissy.ca/">Voir</a>
-                            <a class="btn-secondary mt-3" target="_blank" href="https://github.com/williamLafortuneCaissy/trainingApp">Github</a>
-                        </div>
-                        <div class="col-1-2">
-                            <h3 class="my-0 fw-500">Projet e-commerce</h3>
-                            <div class="">En cours de développent</div>
-                            <a class="btn-primary mt-3" target="_blank" href="https://xd.adobe.com/view/84f947c1-e6d9-43f5-b624-1621cccb19fe-d1aa/?hints=off">Voir</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <?php require_once 'part/home/projects.php' ?>
 
             <section id="cv" class="home__section section_cv">
                 <div class="container container--narrow">
---
+++ part/home/projects.php
@@ -0,0 +1,21 @@
+<section class="home__section text-center">
+    <div class="container container--narrow">
+        <h2 class="fs-4 mb-2 fw-500 sidelines">Projets personnels</h2>
+        <div class="project__row mt-5">
+            <div class="project__col">
+                <img class="project__img" src="img/home/trainingApp.png" alt="Apperçu de l'application d'entrainement">
+                <h3 class="my-0 fw-500">Application mobile d'entrainement</h3>
+                <div class="">App React, vue mobile seulement</div>
+                <a class="btn-primary mt-3" target="_blank" href="http://trainingapp.williamlafortunecaissy.ca/">Voir</a>
+                <a class="btn-secondary mt-3" target="_blank" href="https://github.com/williamLafortuneCaissy/trainingApp">Github</a>
+            </div>
+            <div class="project__separator"></div>
+            <div class="project__col">
+                <img class="project__img" src="img/home/logoecom.png" alt="Logo Ecom">
+                <h3 class="my-0 fw-500">Projet e-commerce</h3>
+                <div class="">En cours de développent</div>
+                <a class="btn-primary mt-3" target="_blank" href="https://xd.adobe.com/view/84f947c1-e6d9-43f5-b624-1621cccb19fe-d1aa/?hints=off">Voir</a>
+            </div>
+        </div>
+    </div>
+</section>
